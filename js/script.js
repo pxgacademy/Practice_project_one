18b6ec493a7b3c41d3a491f5b934e3012fb8c933
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-26 19:00:37 +0600

    Created files and folders

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Tailwindcss cdn
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primaryColor: "#ABEF5F",
+        secondaryColor: "#abef5f80",
+        primaryTextColor: "#212121",
+        secondaryTextColor: "#737373",
+        bgColorOne: "#F9F9F9",
+        bgColorTwo: "#F3F3F3",
+      },
+      backgroundPosition: {
+        customBgPosition: "25px 50px",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+};
+// "use strict";
